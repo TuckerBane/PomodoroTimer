fe01499496e8cac6edbf5270945ffc88bf2bc702
TuckerBane <Tucker.bane@icloud.com>
2018-02-03 18:20:42 -0800

    Pomodoro Timer
    A windows forms application that acts like a Pomodoro timer.

---
+++ WindowsFormsTimer/PomodoroTimer.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsTimer
+{
+    partial class PomodoroTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.TimeRemainingUpdater = new System.Windows.Forms.Timer(this.components);
+            this.TimeRemainingLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
+            this.StartButton.Location = new System.Drawing.Point(161, 132);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(217, 82);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.StartButton_MouseUp);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Location = new System.Drawing.Point(12, 340);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(157, 53);
+            this.PauseButton.TabIndex = 1;
+            this.PauseButton.Text = "Reset Round";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RestartRound_MouseUp);
+            // 
+            // TimeRemainingUpdater
+            // 
+            this.TimeRemainingUpdater.Enabled = true;
+            this.TimeRemainingUpdater.Tick += new System.EventHandler(this.TimeRemainingUpdater_Tick_1);
+            // 
+            // TimeRemainingLabel
+            // 
+            this.TimeRemainingLabel.AutoSize = true;
+            this.TimeRemainingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeRemainingLabel.Location = new System.Drawing.Point(141, 9);
+            this.TimeRemainingLabel.Name = "TimeRemainingLabel";
+            this.TimeRemainingLabel.Size = new System.Drawing.Size(260, 120);
+            this.TimeRemainingLabel.TabIndex = 2;
+            this.TimeRemainingLabel.Text = "0:00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Restart Pattern";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RestartPattern_MouseUp);
+            // 
+            // PomodoroTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 406);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TimeRemainingLabel);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "PomodoroTimer";
+            this.Text = "Pomodoro Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Timer TimeRemainingUpdater;
+        private System.Windows.Forms.Label TimeRemainingLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsTimer/PomodoroTimer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTimer
+{
+    public partial class PomodoroTimer : Form
+    {
+        private TimeSpan internalCurrentTime;
+        private TimeSpan currentTime { get { return internalCurrentTime; }
+                                       set {
+                internalCurrentTime = value;
+                if (TimeRemainingLabel != null)
+                    TimeRemainingLabel.Text = internalCurrentTime.ToString("mm") + ":" + internalCurrentTime.ToString("ss");
+            }
+        }
+
+        private TimeSpan[] seriesOfTimes;
+        private int timeSeriesIndex = 0;
+        private bool timerPaused = true;
+        
+        public PomodoroTimer()
+        {
+            TimeSpan work = new TimeSpan(0, 25, 0);
+            TimeSpan shortBreak = new TimeSpan(0, 5, 0);
+            TimeSpan longBreak = new TimeSpan(0, 15, 0);
+
+            seriesOfTimes = new TimeSpan[] { work, shortBreak, work, shortBreak, work, shortBreak, work, longBreak };
+            currentTime = seriesOfTimes[timeSeriesIndex];
+            InitializeComponent();
+        }
+
+        private void StartButton_MouseUp(object sender, MouseEventArgs e)
+        {
+            TogglePause();
+        }
+
+
+        private void TogglePause()
+        {
+            timerPaused = !timerPaused;
+            if (timerPaused)
+                StartButton.Text = "Start";
+            else
+                StartButton.Text = "Pause";
+        }
+
+        private void TimeRemainingUpdater_Tick_1(object sender, EventArgs e)
+        {
+            if (timerPaused)
+                return;
+            Timer time = sender as Timer;
+            currentTime -= new TimeSpan(0, 0, 0, 0, time.Interval * 100);
+
+            if (currentTime.TotalMilliseconds <= 0)
+            {
+                TogglePause();
+                if (timeSeriesIndex % 2 == 0) // working
+                    MessageBox.Show("Take a break!", "Time's up!", MessageBoxButtons.OK);
+                else
+                    MessageBox.Show("Back to work!", "Time's up!", MessageBoxButtons.OK);
+                TogglePause();
+
+                TimeSpan oldTime = currentTime;
+                ++timeSeriesIndex;
+                timeSeriesIndex %= seriesOfTimes.Length;
+                currentTime = seriesOfTimes[timeSeriesIndex];
+                // so we don't lose time in the transition
+                currentTime += oldTime;
+            }
+        }
+
+        private void RestartPattern_MouseUp(object sender, MouseEventArgs e)
+        {
+            timeSeriesIndex = 0;
+            RestartRound_MouseUp(sender, e);
+        }
+
+        private void RestartRound_MouseUp(object sender, MouseEventArgs e)
+        {
+            currentTime = seriesOfTimes[timeSeriesIndex];
+            if (!timerPaused)
+                TogglePause();
+        }
+    }
+}
